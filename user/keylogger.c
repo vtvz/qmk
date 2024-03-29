8b199a9d02ca510325ceb351f2641fccc7c0953c
Vitaly <vtvz.ru@gmail.com>
2024-03-29 16:08:17 +0300

    remove layer changers for keylogger

--- user/keylogger.c
+++ user/keylogger.c
@@ -1,8 +1,24 @@
 #include "print.h"
 
 void keylogger_process_record(uint16_t keycode, keyrecord_t *record) {
-  uprintf("0x%04X,%u,%u,%u,%b,0x%02X,0x%02X,%u\n", keycode,
-          record->event.key.row, record->event.key.col,
-          get_highest_layer(layer_state), record->event.pressed, get_mods(),
-          get_oneshot_mods(), record->tap.count);
+  if (IS_QK_LAYER_TAP(keycode) && (record->tap.count == 0)) {
+    return;
+  }
+
+  if (keycode == TL_LOWR || keycode == TL_UPPR) {
+    return;
+  }
+
+  // clang-format off
+  uprintf("0x%04X,%u,%u,%u,%b,0x%02X,0x%02X,%u\n",
+    keycode,
+    record->event.key.row,
+    record->event.key.col,
+    get_highest_layer(layer_state),
+    record->event.pressed,
+    get_mods(),
+    get_oneshot_mods(),
+    record->tap.count
+  );
+  // clang-format on
 }
